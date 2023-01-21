5e7db82621f97c6a952725f3f4db91032783edb7
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-01-21 06:23:38 +0200

    complete task8

---
+++ js/task-08.js
@@ -0,0 +1,16 @@
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+  const formObj = { email: email.value, password: password.value };
+  console.log(formObj);
+  event.currentTarget.reset();
+}
